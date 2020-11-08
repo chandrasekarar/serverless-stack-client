bdd8999389fb35fdaeaf0a0fd493c98a24e25337
Chandrasekara Reddy M <csrias@gmail.com>
2020-11-08 19:27:16 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-south-1",
+    BUCKET: "notes-app-uploads-serverless-stack-learn",
+  },
+  apiGateway: {
+    REGION: "ap-south-1",
+    URL: "https://4749g3o662.execute-api.ap-south-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "ap-south-1",
+    USER_POOL_ID: "ap-south-1_k0KqweHhB",
+    APP_CLIENT_ID: "73vpqd4mfmlcomg4m0aqqiatbu",
+    IDENTITY_POOL_ID: "ap-south-1:9ba900b4-9557-432a-9a60-9a954294b595",
+  },
+};
